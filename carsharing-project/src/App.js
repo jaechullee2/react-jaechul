acc0c64bb2e65b5516566ca3286208f146b292ce
Jayden <chel8780@gmail.com>
2023-03-21 16:21:22 +0900

    inputSample ref Upload

--- carsharing-project/src/App.js
+++ carsharing-project/src/App.js
@@ -1,10 +1,12 @@
 import UseStateTest from './Component/test/useStateTest';
 import InputSample from './Component/test/inputSample';
+import InputSample2 from './Component/test/inputSample2';
 function App() {
   return (
     <div className="App">
       <UseStateTest name='이재철' />
       <InputSample />
+      <InputSample2 />
     </div>
   );
 }
