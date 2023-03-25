de15515e688b97df90b7e7eac1fa8c1341cbebde
Jayden <chel8780@gmail.com>
2023-03-25 15:28:26 +0900

    UserList Upload

--- carsharing-project/src/App.js
+++ carsharing-project/src/App.js
@@ -1,12 +1,14 @@
 import UseStateTest from './Component/test/useStateTest';
 import InputSample from './Component/test/inputSample';
 import InputSample2 from './Component/test/inputSample2';
+import UserList from './Component/test/UserList';
 function App() {
   return (
     <div className="App">
       <UseStateTest name='이재철' />
       <InputSample />
       <InputSample2 />
+      <UserList />
     </div>
   );
 }
