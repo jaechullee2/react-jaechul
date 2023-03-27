c5172bddded25ae0e2f20f79dbfd532bb0c7fdc3
Jayden <chel8780@gmail.com>
2023-03-27 19:13:44 +0900

    useRef Upload

--- carsharing-project/src/App.js
+++ carsharing-project/src/App.js
@@ -1,14 +1,40 @@
+import React, { useRef } from 'react';
 import UseStateTest from './Component/test/useStateTest';
 import InputSample from './Component/test/inputSample';
 import InputSample2 from './Component/test/inputSample2';
 import UserList from './Component/test/UserList';
 function App() {
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+  const nextId = useRef(4);
+  const onCreate = () => {
+    // 나중에 구현 할 배열에 항목 추가하는 로직
+    // ...
+
+    nextId.current += 1;
+  };
   return (
     <div className="App">
       <UseStateTest name='이재철' />
       <InputSample />
       <InputSample2 />
       <UserList />
+      <UserList users={users} />;
     </div>
   );
 }
