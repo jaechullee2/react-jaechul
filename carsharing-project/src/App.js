4daae8ead9152ee0858686e247590176611290b0
Jayden <chel8780@gmail.com>
2023-03-28 22:43:46 +0900

    CreateUser Upload

--- carsharing-project/src/App.js
+++ carsharing-project/src/App.js
@@ -1,10 +1,23 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import UseStateTest from './Component/test/useStateTest';
 import InputSample from './Component/test/inputSample';
 import InputSample2 from './Component/test/inputSample2';
 import UserList from './Component/test/UserList';
+import CreateUser from './Component/test/CreateUser';
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username:'',
+    email:''
+});
+const {username, email} = inputs; //비구조화 할당을 통해 값 추출
+const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+        ...inputs, //기존의 input 객체를 복사한 뒤
+        [name]: value //name 키를 가진 값을 value로 설정
+    });
+};
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -20,22 +33,37 @@ function App() {
       username: 'liz',
       email: 'liz@example.com'
     }
-  ];
+  ]);
   const nextId = useRef(4);
   const onCreate = () => {
-    // 나중에 구현 할 배열에 항목 추가하는 로직
-    // ...
+    const user={
+      id : nextId.current,
+      username,
+      email
+    };
+    setUsers([...users, user]);
 
+    setInputs({
+      username: '',
+      email: ''
+    });
     nextId.current += 1;
   };
   return (
-    <div className="App">
-      <UseStateTest name='이재철' />
+    <>
+      {/* <UseStateTest name='이재철' />
       <InputSample />
       <InputSample2 />
-      <UserList />
-      <UserList users={users} />;
-    </div>
+      <UserList /> */}
+      {/* <CreateUser /> */}
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
   );
 }
 
