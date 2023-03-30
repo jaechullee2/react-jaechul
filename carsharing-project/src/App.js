85d539aaeb55bafcdaac143cf8e58dc60daf78c2
Jayden <chel8780@gmail.com>
2023-03-30 21:36:25 +0900

    react memo,  useCallback Upload

--- carsharing-project/src/App.js
+++ carsharing-project/src/App.js
@@ -1,22 +1,28 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useMemo, useCallback } from 'react';
 import UseStateTest from './Component/test/useStateTest';
 import InputSample from './Component/test/inputSample';
 import InputSample2 from './Component/test/inputSample2';
 import UserList from './Component/test/UserList';
 import CreateUser from './Component/test/CreateUser';
+function countAtiveUser(users){
+  console.log('활성 사용자 수를 세는 중..');
+  return users.filter(user => user.active).length;
+}
 function App() {
   const [inputs, setInputs] = useState({
     username:'',
     email:''
 });
 const {username, email} = inputs; //비구조화 할당을 통해 값 추출
-const onChange = e => {
+const onChange = useCallback( e => {
     const { name, value } = e.target;
     setInputs({
         ...inputs, //기존의 input 객체를 복사한 뒤
         [name]: value //name 키를 가진 값을 value로 설정
     });
-};
+  },
+  [inputs]
+);
   const [users, setUsers] = useState([
     {
       id: 1,
@@ -35,7 +41,7 @@ const onChange = e => {
     }
   ]);
   const nextId = useRef(4);
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user={
       id : nextId.current,
       username,
@@ -48,12 +54,24 @@ const onChange = e => {
       email: ''
     });
     nextId.current += 1;
-  };
-  const onRemove = id => {
+  }, [users,username, email]);
+
+  const onRemove = useCallback(
+  id => {
     // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
     // = user.id 가 id 인 것을 제거함
     setUsers(users.filter(user => user.id !== id));
+  },
+  [users]
+  );
+  const onToggle = id => {
+    setUsers(
+      users.map(user =>
+        user.id === id ? { ...user, active : !user.active} :user
+        )
+    );
   };
+  const count = useMemo(()=>countAtiveUser(users), [users]);
   return (
     <>
       {/* <UseStateTest name='이재철' />
@@ -67,7 +85,8 @@ const onChange = e => {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove}/>
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <div>활성사용자 수 : {count}</div>
     </>
   );
 }
