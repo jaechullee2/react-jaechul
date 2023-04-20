3118d36e14b77ae1669d9c5c5844b44d9318a02c
Jayden <chel8780@gmail.com>
2023-04-20 18:52:06 +0900

    java code Upload

---
+++ carsharing-project/src/test/java.js
@@ -0,0 +1,19 @@
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+    // Getters and Setters
+}
